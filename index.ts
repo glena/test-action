db2632dee8be7c757d0445be76a52c3dde9dbeda
Germán Lena <v-german@pulumi.com>
2024-04-22 09:43:26 -0700

    comment aws stuff

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import * as aws from "@pulumi/aws";
+// import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.Bucket("my-bucket", {
-  acl: "private",
-  tags: {
-    Environment: "Dev",
-    Name: "My bucket",
-  },
-});
+// const bucket = new aws.s3.Bucket("my-bucket", {
+//   acl: "private",
+//   tags: {
+//     Environment: "Dev",
+//     Name: "My bucket",
+//   },
+// });
 
-export const bucketName = bucket.id;
+// export const bucketName = bucket.id;
 
 export const someOutput = "the value2";
