4803f395ba4b4ffcf984f91d10d8ac8961878667
German <v-german@pulumi.com>
2024-07-10 10:44:01 -0300

    a

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Create a Google Cloud resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US",
-});
-
+// // Create a Google Cloud resource (Storage Bucket)
+// const bucket = new gcp.storage.Bucket("my-bucket", {
+//     location: "US",
+// });
+console.log("hola")
 // Export the DNS name of the bucket
-export const bucketName = bucket.url;
+export const bucketName = "bleh";
