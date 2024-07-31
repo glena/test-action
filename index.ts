593f68cc803d7cec82f22cc4c2fa6f2813831813
German <v-german@pulumi.com>
2024-07-31 18:02:19 -0300

    dump

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 
+console.log(process.env)
+console.log(process.env.secret.split())
+
 // Export the DNS name of the bucket
 export const bucketName = "dummy";
