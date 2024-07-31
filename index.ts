c0447a0e027db3692e61b81b3bfc78ea355d6a88
German <v-german@pulumi.com>
2024-07-31 15:34:21 -0300

    init

--- index.ts
+++ index.ts
@@ -1,10 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
-
-// Create a Google Cloud resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US",
-});
 
 // Export the DNS name of the bucket
-export const bucketName = bucket.url;
+export const bucketName = "dummy";
